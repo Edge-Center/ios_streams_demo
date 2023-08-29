854a1b878bd8753f158e9a9b50bc25822234e506
Maksim Myakishev <maksim.myakishev@edgecenter.ru>
2023-08-29 14:26:15 +0700

    Endpoints changed

--- EdgeCenterStreamsDemo/Network/EdgeCenterApi.swift
+++ EdgeCenterStreamsDemo/Network/EdgeCenterApi.swift
@@ -3,6 +3,6 @@ import Foundation
 enum EdgeCenterApi: String {
     case authorization = "https://api.edgecenter.ru/iam/auth/jwt/login"
     case refreshToken = "https://api.edgecenter.ru/iam/auth/jwt/refresh"
-    case streams = "https://api.edgecenter.ru/streaming/streams"
-    case broadcasts = "https://api.edgecenter.ru/streaming/broadcasts"
+    case streams = "https://api.edgecenter.ru/streaming/live/streams"
+    case broadcasts = "https://api.edgecenter.ru/streaming/live/multicameras"
 }
